06847423de710a73a79e27eaba886f6cd2c7657d
Ted Kuo <t1162625@gmail.com>
2021-06-16 20:59:40 +0800

    feat: 處理讀取效果 / 處理表單驗證
    1. index.html
    2. app.js，修改 addCart,  updateCart, 以及表單驗證元件

--- week5/js/app.js
+++ week5/js/app.js
@@ -42,15 +42,19 @@ const app = Vue.createApp({
       // ? 為什麼上面已經傳入 item 了，卻不直接拿來用
       // * 因為要避免單向數據流的問題，所以盡可能不要去使用外層傳進來的資料
       // * 之前是因為丟進去的是物件，有傳參考的特性
+
+      this.loadingStatus.loadingItem = item.id;
       const url = `${apiUrl}/api/${apiPath}/product/${item.id}`;
       axios.get(url)
         .then((res) => {
           console.log(res);
           this.product = res.data.product;
+          this.loadingStatus.loadingItem = '';
           this.$refs.userProductModal.openModal();
         })
     },
     addCart(id, qty=1) {
+      this.loadingStatus.loadingItem = id;
       const cart = {
         product_id: id,
         qty
@@ -60,10 +64,12 @@ const app = Vue.createApp({
       axios.post(url, {data: cart})
         .then((res) => {
           console.log(res);
+          this.loadingStatus.loadingItem = '';
           this.getCart();
         })
     },
     getCart() {
+
       const url = `${apiUrl}/api/${apiPath}/cart`;
       axios.get(url)
         .then((res) => {
@@ -71,6 +77,7 @@ const app = Vue.createApp({
         })
     },
     updateCart(item) {
+      this.loadingStatus.loadingItem = item.id;
       const url = `${apiUrl}/api/${apiPath}/cart/${item.id}`;
       const cart = {
         product_id: item.product.id,
@@ -79,6 +86,7 @@ const app = Vue.createApp({
       console.log(cart, url)
       axios.put(url, {data: cart})
         .then((res) => {
+          this.loadingStatus.loadingItem = '';
           console.log(res);
         })
     }
@@ -91,6 +99,23 @@ const app = Vue.createApp({
   },
 })
 
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zh_TW'),
+  validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
+
+Object.keys(VeeValidateRules).forEach(rule => {
+  if (rule !== 'default') {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
 app.component('userProductModal', productModal);
 app.mount('#app');
